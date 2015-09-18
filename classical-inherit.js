33120fffe7978ae2d02fc8804b807660d908c419
Владислав Хвостов <vladkhvo@gmail.com>
2015-09-18 08:11:57 +0300

    Update classical-inherit.js

--- classical-inherit.js
+++ classical-inherit.js
@@ -82,13 +82,14 @@ function inherit(C, P){
 // classical inherit # (preferred way)
 
 
-var inherit = (function(){
-	var F = functuion() {};
-	return function(C, P){
+var inherit = (function () {
+	var F = function () {};
+	return function (C, P) {
 		F.prototype = P.prototype;
 		C.prototype = new F();
 		C.uber = P.prototype;
 		C.prototype.constructor = C;
 	}
-})
+})();
+
 
