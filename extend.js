29fa76e9dd249c24530d7b0d2f855de8ac32d42a
Владислав Хвостов <vladkhvo@gmail.com>
2015-09-25 08:53:41 +0300

    Function that copies objects.
    //  Function that copies objects and their properties.
    // Doesn't work with prototypes.

--- extend.js
+++ extend.js
@@ -3,10 +3,30 @@
 
 function extend(parent, child) {
   var i;
-  var child = child || {};
+  child = child || {};
   for(i in parent) {
     if(parent.hasOwnProperty(i)) {
       child[i] = parent[i];
+    }
+  }
+  return child;
+}
+
+function extendDeep(parent, child) {
+  var i;
+  var toStr = Object.prototype.toString;
+  var astr = '[object Array]';
+  
+  child = child || {};
+  
+  for(i in parent) {
+    if (parent.hasOwnProperty(i)) {
+      if (typeof parent[i] === 'object') {
+        child[i] = (toStr.call(parent[i]) === astr) ? [] : {};
+        extendDeep(parent[i], child[i]);
+      } else {
+        child[i] = parent[i];
+      }
     }
   }
   return child;
