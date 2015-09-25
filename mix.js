29491784e9aa93419a656abc259b8364385e6a45
Владислав Хвостов <vladkhvo@gmail.com>
2015-09-25 10:10:41 +0300

    Analog Object.assign() in ECMASCRIPT 6.
    // Analog Object.assign() in ECMASCRIPT 6.
    // Copying all properties from list of objects to one.

--- mix.js
+++ mix.js
@@ -6,7 +6,7 @@ function mix() {
   var prop;
   child = {};
   
-  for(arg = 0; arg < arguments.length; atg += 1) {
+  for(arg = 0; arg < arguments.length; arg += 1) {
     for(prop in arguments[arg]) {
       if(arguments[arg].hasOwnProperty(prop)) {
         child[prop] = arguments[arg][prop];
