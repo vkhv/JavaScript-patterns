62fbdec1ee97053f3e15fbe2b328f54fab3d9483
Владислав Хвостов <vladkhvo@gmail.com>
2015-10-05 08:39:13 +0300

    Create singelton.js

---
+++ singelton.js
@@ -0,0 +1,26 @@
+// The Singleton Pattern limits the number of instances of a particular object to just one. 
+// http://www.dofactory.com/javascript/singleton-design-pattern
+
+//The instances in clouser.
+
+function Singelton() {
+  var instance = this;
+  
+  Singelton = function() {
+    return instance
+  };
+  
+  Singelton.prototype = this;
+  
+  instance = new Singelton();
+
+  instance.constructor = Singelton;
+  
+  return instance;
+}
+
+var x  = new Singelton();
+x.someProp = true;
+var y = new Singelton();
+y.someProp = false;
+x.someProp // false
